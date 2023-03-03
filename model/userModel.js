972867ba4a1deb64cfb3521afa4f8e5bd4ef420d
iam <iam@gmail.com>
2023-03-03 00:21:13 -0800

    created user model

---
+++ model/userModel.js
@@ -0,0 +1,27 @@
+const{model,Schema}=require("mongoose");
+const userSchema=new Schema({
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    balance:{
+        type:Number,
+        required:true
+    }
+});
+
+const userModel=model("user",userSchema);
+
+module.exports=userModel;
