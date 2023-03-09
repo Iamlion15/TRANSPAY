5a8546934f76c82ea326bb2e1fffee880b803e66
iam <iam@gmail.com>
2023-03-09 00:56:14 -0800

    added withDrawal

--- middlewares/accountValidation.js
+++ middlewares/accountValidation.js
@@ -1,7 +1,7 @@
 const accountModel = require("../model/accountModel");
 const userModel = require("../model/userModel");
 
-const validateAmount = async(req, res, next) => {
+const validateTopup = async(req, res, next) => {
     const amount = req.body.amount;
     const phone = req.user;
     const accountuser = await userModel.findOne({ phoneNumber: phone });
@@ -19,4 +19,27 @@ const validateAmount = async(req, res, next) => {
     }
 }
 
-module.exports = validateAmount;
+const validateWithdrawal = async(req, res, next) => {
+    const amount = req.body.amount;
+    const phone = req.user;
+    const accountuser = await userModel.findOne({ phoneNumber: phone });
+    const account = await accountModel.findOne({ user: accountuser._id });
+    const newAmount = account.balance - amount;
+    if (amount < 0) {
+        res.status(200).json({ "message": "amount can not be negative" });
+    }
+    else if (amount > account.balance) {
+        res.status(200).json({ "message": "not enough funds" });
+    }
+    else if(amount ==account.balance){
+        res.status(200).json({"message":"you can not leave balance to 0"})
+    }
+    else {
+        req.amount = newAmount;
+        next();
+    }
+}
+
+
+
+module.exports = {validateTopup,validateWithdrawal};
